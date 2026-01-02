aaf1c4b51b9851fac7671598a9fe5704d5bb3a96
nkahz <khanhkhanhok33@gmail.com>
2026-01-03 00:22:51 +0700

    Update ezbounty.lua

--- ezbounty.lua
+++ ezbounty.lua
@@ -1,4 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-BountyEz.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nk210/blocphut/refs/heads/main/fixsus.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nk210/blocphut/refs/heads/main/autoclicker.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nk210/blocphut/refs/heads/main/walkonwater.lua"))()
