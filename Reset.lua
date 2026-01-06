f4c3e3ae4a398122aa6cfe3491d96724011af122
nkahz <khanhkhanhok33@gmail.com>
2026-01-06 17:57:03 +0700

    Update Reset.lua

--- Reset.lua
+++ Reset.lua
@@ -1,16 +1,25 @@
 local VIM = game:GetService("VirtualInputManager")
 
-local function fastReset()
-    -- Nhấn phím R để mở menu reset
+local function fullReset()
+    -- 1. Nhấn ESC để mở Menu chính
+    VIM:SendKeyEvent(true, Enum.KeyCode.Escape, false, game)
+    task.wait(0.1)
+    VIM:SendKeyEvent(false, Enum.KeyCode.Escape, false, game)
+    
+    task.wait(0.1) -- Đợi menu hiện lên
+
+    -- 2. Nhấn R để chọn lệnh Reset
     VIM:SendKeyEvent(true, Enum.KeyCode.R, false, game)
-    task.wait(0.05)
+    task.wait(0.1)
     VIM:SendKeyEvent(false, Enum.KeyCode.R, false, game)
     
-    task.wait(0.1) -- Đợi menu hiện ra một chút
-    
-    -- Nhấn Enter để xác nhận Reset
+    task.wait(0.1) -- Đợi bảng xác nhận hiện lên
+
+    -- 3. Nhấn Enter (Return) để xác nhận Reset
     VIM:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
-    task.wait(0.05)
+    task.wait(0.1)
     VIM:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
 end
 
+-- Chạy lệnh reset
+fullReset()
